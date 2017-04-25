e6705db5d9f71f76f2adc05eb34612c59145097b
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-04-25 00:27:54 -0500

    add input with update functionality for search_bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReacDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
 // create a new component. this component should produce some HTML
-
 const API_KEY = 'AIzaSyB-T5NfPqLvor0VIEYQCfdAMGJIkinBiNo';
 
-const App = () => {
-  return <div>Hi !</div>; // JSX
+/*const App = () => {
+return (
+<div>
+<SearchBar />
+</div>
+); // JSX
+}*/
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'bmx' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 // take this component's HTML and put it in the DOM
 
