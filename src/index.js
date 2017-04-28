b7efdfb7d8532f61824e8c67b975b5faaa2bd69f
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-04-28 00:06:30 -0500

    add functionality to search and show videos

--- src/index.js
+++ src/index.js
@@ -1,32 +1,50 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReacDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 // create a new component. this component should produce some HTML
 const API_KEY = 'AIzaSyB-T5NfPqLvor0VIEYQCfdAMGJIkinBiNo';
 
 /*const App = () => {
 return (
-<div>
-<SearchBar />
-</div>
+  <div>
+    <SearchBar />
+  </div>
 ); // JSX
 }*/
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: 'bmx' }, (videos) => {
-      this.setState({ videos });
+    this.videoSearch('bmx');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return(
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
       </div>
     );
   }
